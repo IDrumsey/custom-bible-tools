e996e9ff8c2d541ae8f7d79bdc326fc0f201167c
IDrumsey <claydevelop@gmail.com>
2023-02-25 15:27:38 -0500

    Added users page

---
+++ pages/users.tsx
@@ -0,0 +1,43 @@
+import { supabase } from "@lib/supabaseClient"
+import { useEffect, useState } from "react"
+import Link from 'next/link'
+
+interface Props {
+
+}
+
+const UsersPage = ({
+
+}: Props) => {
+
+    const [users, usersSetter] = useState<any[]>([])
+
+    useEffect(() => {
+
+        const handleLoad = async() => {
+            const { data } = await supabase.from('profiles').select()
+            if(data) {
+                usersSetter(data)
+            }
+        }
+
+        handleLoad()
+
+    }, [])
+
+    return (
+        <>
+        {
+            users && users.map((user, i) => {
+                return (
+                    <Link href={`/profiles/${user.id}`} key={i}>
+                        {user.first_name} {user.last_name}
+                    </Link>
+                )
+            })
+        }
+        </>
+    )
+}
+
+export default UsersPage
